5f56713e7c13727e2a25a362388a2132c6866e17
GrzegorzZapala <zapala.grzegorz@gmail.com>
2018-02-24 19:17:56 +0100

    working version

--- src/app.js
+++ src/app.js
@@ -3,57 +3,69 @@ import ReactDOM from 'react-dom';
 import marked from 'marked';
 import 'Styles/style.scss';
 
+// helper function, not a functional component
 function createMarkup(arg) {
+  // zwraca obiekt z property __html
   return {__html: arg};
 }
 
-function MyComponent(props) {
-  return <div dangerouslySetInnerHTML={createMarkup(props.value)}  />;
+// functional component
+// https://reactjs.org/docs/components-and-props.html
+function MDoutput(props) {
+  // dangerouslySetInnerHTML to JSX attribute
+  // który pozwala na dynamiczne określanie markup'u
+  // czemu nie można przekazać gotowego html'a? Nie wiem, dokumentacja przekazuje to do funkcji
+  return <div className="MDRenderer__output" dangerouslySetInnerHTML={createMarkup(props.value)}  />;
 }
-class EssayForm extends React.Component {
+
+// gdy komponent klasowy odbiera props, to nie musi mieć zdefiniowanej właściwość props
+// tylko gdy nie ma własnego stanu -> nie ma własnego konstruktora
+// https://codepen.io/gaearon/pen/zKRGpo?editors=0010
+class App extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        value: '',
+        // value: '',
         code: ''
       };
-      // why bind(this)?
+      // ?!
       this.handleChange = this.handleChange.bind(this);
     }
   
     handleChange(event) {
+      let newHtml = marked(event.target.value);
       this.setState(
         {
-          code: marked(this.state.value),
-          value: event.target.value
+          code: newHtml,
+          // value: event.target.value
         }
         
       );
     }
   
-    handleSubmit(event) {
-      alert('An essay was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
     render() {
       return (
-          <div className="grid-x padding-x">
-            <div className="cell small-6"> 
-              <textarea value={this.state.value} onChange={this.handleChange} />
-            </div>
-            <div className="cell small-6"> 
-             <MyComponent value={this.state.code}/>
+        <section className ="MDRenderer__wrap">
+          <h1 className="text-center MDRenderer__header">Markdown previewer (fcc)</h1>
+          <div className="grid-container">
+            <div className="grid-x grid-padding-x MDRenderer">
+              <div className="cell small-12 medium-6"> 
+                <textarea  className="MDRenderer__input" /* value={this.state.value} */ onChange={this.handleChange} />
+              </div>
+              <div className="cell small-12 medium-6"> 
+              <MDoutput  value={this.state.code}/>
+              </div>
             </div>
           </div>
+        </section>
           );
     }
-  }
+}
 
 // ========================================
 
 ReactDOM.render(
-    <EssayForm />,
+    <App />,
     document.getElementById('root'),
 );
 
